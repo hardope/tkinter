6db2203e7827e0e5269eaf9f9111364a25b6dedb
Opeoluwa Adeyeri <adeyeriopeoluwa05@gmail.com>
2023-01-04 14:22:32 +0100

    Update

--- survey.py
+++ survey.py
@@ -27,7 +27,8 @@ def questions(element, element1, window):
                     file.write(f"Name: {name_field.get()}\n")
                     file.write(f"Age: {age.get()}\n")
                     file.write(f"Sex: {sex.get()}\n")
-                    file.write(f"Race: {race.get()}\n")
+                    file.write(f"Ethnicity: {ethnicity.get()}\n")
+                    file.write(f"01: {question1.get()}\n")
                a = True
           else:
                pass
@@ -64,31 +65,53 @@ def questions(element, element1, window):
      male.place(x=400, y=190)
      female.place(x=400, y=210)
 
-     # Label For Race Option
-     race_label = Label(window, text="Race", font=(20))
-     race_label.place(x=495, y=240)
+     # Label For ethnicity Option
+     ethnicity_label = Label(window, text="Ethnicity", font=(20))
+     ethnicity_label.place(x=495, y=240)
 
-     # Variable for race options
-     race = StringVar()
-     race.set("black")
+     # Variable for ethnicity options
+     ethnicity = StringVar()
+     ethnicity.set("black")
 
-     # Radio buttons To choose race
-     black = Radiobutton(window, text="Black", variable=race, value="black")
-     white = Radiobutton(window, text="White", variable=race, value="white")
-     asian = Radiobutton(window, text="Asian", variable=race, value="asian")
-     other = Radiobutton(window, text="Other", variable=race, value="other")
+     # Radio buttons To choose ethnicity
+     black = Radiobutton(window, text="Black", variable=ethnicity, value="black")
+     white = Radiobutton(window, text="White", variable=ethnicity, value="white")
+     asian = Radiobutton(window, text="Asian", variable=ethnicity, value="asian")
+     other = Radiobutton(window, text="Other", variable=ethnicity, value="other")
 
-     # Place Race radio buttons in View
+     # Place ethnicity radio buttons in View
      black.place(x=400, y=260)
      white.place(x=400, y=280)
      asian.place(x=400, y=300)
      other.place(x=400, y=320)
 
+     # Question1: Did You Enjoy The sculpture
+     question1_label = Label(window, text="Did You Enjoy The sculpture", font=(20))
+     question1_label.place(x=430, y=340)
+
+     # Variable for ethnicity options
+     question1 = StringVar()
+     question1.set("agree")
+
+     # Radio buttons To choose ethnicity
+     q1_sa = Radiobutton(window, text="Strongly Agree", variable=question1, value="strongly agree")
+     q1_a = Radiobutton(window, text="Agree", variable=question1, value="agree")
+     q1_nad = Radiobutton(window, text="Neither Agree Nor Disagree", variable=question1, value="neutral")
+     q1_d = Radiobutton(window, text="Disagree", variable=question1, value="disagree")
+     q1_sd = Radiobutton(window, text="Strongly Disagree", variable=question1, value="strongly disagree")
+
+     # Place ethnicity radio buttons in View
+     q1_sa.place(x=400, y=370)
+     q1_a.place(x=400, y=390)
+     q1_nad.place(x=400, y=410)
+     q1_d.place(x=400, y=430)
+     q1_sd.place(x=400, y=450)
+
 
 
      # submit Button
      submit = Button(window, text="Submit", fg="Black", bg="lightblue", command=fetch)
-     submit.place(x=500, y=500)
+     submit.place(x=500, y=600)
 
 def main():
      # create a GUI window
